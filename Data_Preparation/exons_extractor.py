dfe94d45e8e6ec4c0dc286eb23950a0f60222130
StePoli-00 <poli14.sp@gmail.com>
2024-04-18 23:25:12 +0200

    update exons and introns extractor

--- Data_Preparation/exons_extractor.py
+++ Data_Preparation/exons_extractor.py
@@ -1,36 +1,45 @@
 import json
 import argparse
-gtf_path="gencode.v45.annotation.gtf"
-fasta_path="GRCh38.p14.genome.fa"
+gtf_path=""
 output_file=""
 
 
 i=0
 chromosomes=[]
-exons_position={}
 
 
 
-def save_exons():        
+def save_exons(data):        
     with open(output_file+".json","w") as f:
-        json.dump(exons_position,f,indent=1)
+        json.dump(data,f,indent=2)
 
 
 def extract_exons():
+    exons=[]
+    exons_position={}
+    prev=""
+    curr=""
     with open(gtf_path,"r") as f:
+        #skip header
         for k in range(5):
             line=f.readline()
         while(True):
-            init_pos=""
-            end_pos=""
+           
             line=f.readline()
-
             data=line.split("\t")
             if data[0] not in chromosomes:
+                exons_position[prev]=exons
                 break
 
+            curr=data[0]
+            if prev=="":
+                prev=curr
+            elif prev!=curr:
+                exons_position[prev]=exons
+                exons.clear()
+
             if data[2]=="transcript":
-                exons=[]
+                
                 while(True):
                     line=f.readline()
                     ex_data=line.split("\t")
@@ -39,8 +48,8 @@ def extract_exons():
                     
                     else:
                         break
-                exons_position[i]=exons
-                i+=1
+            prev=curr
+    return exons_position
                 
 def parse_chromosome_list(chrs):
     return [ ch for ch in chrs.split(",")]
@@ -48,13 +57,11 @@ def parse_chromosome_list(chrs):
 if __name__=="__main__":
                 
     parser=argparse.ArgumentParser()
-    parser.add_argument("-gr","--genome_reference",help="genome fasta file path (.fa)")
     parser.add_argument("-gtf","--genome_annotation",help="genome annotation file reference (.gtf)")
     parser.add_argument("-out","--output_file",help="output file basename")
     parser.add_argument("-ch","--chromosomes",type=parse_chromosome_list,help="list of chomosome to extract")
     
     args=parser.parse_args()
-    fasta_path=args.genome_reference
     gtf_path=args.genome_annotation
     output_file=args.output_file
     chromosomes=args.chromosomes
@@ -63,10 +70,8 @@ if __name__=="__main__":
         parser.print_help()
         parser.error("Parameter Error")
     
-    print(gtf_path)
-    print(fasta_path)
-    print(output_file)
-    print(chromosomes)
+    data=extract_exons()
+    save_exons(data)
 
     
     
--- Data_Preparation/introns_extractor.py
+++ Data_Preparation/introns_extractor.py
@@ -1,43 +1,78 @@
 import json
+from Bio import SeqIO
 from Bio.Seq import Seq
 import csv
 import pandas as pd
-introns_pos={}
-genomefile="GRCh38.p14.genome.fa"
-with open("intron_position_1.json","r") as f:
-    introns_pos=json.load(f)
+import argparse
 
-sequence=""
-with open("chr1.txt","r") as f:
-    sequence=Seq(f.read().strip())
 
-# print(sequence)
-# print(f"len of sequence: {len(sequence)} ")
-j=0
-introns=[]
-print(type(sequence))
-for k,v in introns_pos.items():
-    #print(f"id{k} num of exon {len(v)}")
-    for i in range(len(v)-1):
-        _,begin,strand=v[i]
-        end,_,strand=v[i+1]
-       
-        if strand=="-":
-            #print(f"id:{k} begin:{end} end:{begin} strand {strand}")
-            subs=sequence[int(end)-1:int(begin)-1]
-            subs=Seq.reverse_complement(subs)
-        else:
-            #print(f"id:{k} begin:{begin} end:{end} strand {strand}")
-            subs=sequence[int(begin)-1:int(end)-1]
-       
+exons={}
+genomefile=""
+exon_file=""
+intron_file=""
 
-        if len(subs)>=1000:
-            subs=subs[0:1000]
-        introns.append(subs)
 
-print(len(introns))    
-with open("intron_1.csv","w") as f:
-    f.write("Seq,\n")
-    for intr in introns:
-        f.write(f"{intr},\n")
+def save_introns(introns):   
+    with open(intron_file+".csv","w") as f:
+        f.write("Seq,\n")
+        for intr in introns:
+            f.write(f"{intr},\n")
+
+def extract_introns():
+
+    with open(exon_file,"r") as f:
+        exons=json.load(f)
+
+    chromosome=list(exons.keys())
+
+    for record in SeqIO.parse(genomefile,"fasta"):
+        
+        if record.id not in chromosome:
+            break
+        sequence=record.seq
+        
+        introns=[]
+        exon_list=exons[record.id]
+        for i in range(len(exon_list)-1):
+            _,begin,strand=exon_list[i]
+            end,_,strand=exon_list[i+1]
+        
+            if strand=="-":
+                #print(f"id:{k} begin:{end} end:{begin} strand {strand}")
+                subs=sequence[int(end)-1:int(begin)-1]
+                subs=Seq.reverse_complement(subs)
+            else:
+                #print(f"id:{k} begin:{begin} end:{end} strand {strand}")
+                subs=sequence[int(begin)-1:int(end)-1]
+
+            """taking all the exon of one chromosome and extract introns in this pattern
+            may happend that exon very far are treated as closer, since they are inside the same list.
+            luckily the slicing produce no output because the begin is and higher pos then end"""
+            if len(subs)!=0:
+                if len(subs)>=1000:
+                    subs=subs[0:1000]
+                
+                introns.append(subs)
+    
+    print(f"{len(introns)} introns extracted")
+    return introns
+
+if __name__=="__main__":
+                
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-gr","--genome_reference",help="genome fasta file path (.fa)")
+    parser.add_argument("-ex","--exon",help="exon file (.json)")
+    parser.add_argument("-out","--output_file",help="output introns file basename")
+   
+    args=parser.parse_args()
+    genomefile=args.genome_reference
+    exon_file=args.exon
+    intron_file=args.output_file
+    
+    if args==None or genomefile==None or exon_file==None or intron_file==None:
+        parser.print_help()
+        parser.error("Parameter Error")
+    
+    introns=extract_introns()
+    save_introns(introns)
 
